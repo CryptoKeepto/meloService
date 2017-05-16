52d420125e881aa4115d924ba6f2343ff5552adf
sittikiat <miikeguru@gmail.com>
2017-05-16 17:31:19 +0700

    สามารถหา โดยใช้ id ได้

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,26 @@ var router = express.Router();
 var connection = require('../module/connect');
 
 /* GET  listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('api');
 });
 
-router.get('/menuFood', function(req, res, next) {
+router.get('/menuFood', function(req, res) {
     connection.query('SELECT * FROM menu_food', function(error, result) {
       if (error) {
           console.log('ไม่สามารถดึงข้อมูล menu_food ได้');
-          throw error
+          throw error;
+      }
+      res.json(result);
+     });
+});
+//ค้นหาตาม id
+router.get('/menuFood/:id', function(req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM menu_food WHERE food_id =' + id, function(error, result) {
+      if (error) {
+          console.log('ไม่สามารถดึง id menu_food ได้');
+          throw error;
       }
       res.json(result);
      });
@@ -27,6 +38,17 @@ router.get('/resFood', function(req, res) {
     });
 });
 
+router.get('/resFood/:id', function(req, res) {
+    let id = req.params.id;
+    connection.query(`SELECT * FROM res_food WHERE resFood_id =${id}`, function(error, result) {
+        if (error) {
+            console.log('ไม่สามารถดึง id res_food ได้');
+            throw error;
+        }
+        res.json(result);
+    })
+});
+
 router.get('/user', function(req, res) {
     connection.query('SELECT * FROM user', function(error, result) {
         if (error) {
@@ -37,8 +59,30 @@ router.get('/user', function(req, res) {
     });
 });
 
+router.get('/user/:id', function(req, res) {
+    let id = req.params.id;
+    connection.query(`SELECT * FROM user WHERE user_id=${id}`, function(error, result) {
+        if (error) {
+            console.log('ไม่สามารถดึงข้อมูล user ได้');
+            throw error;  
+        }
+        res.json(result);
+    });
+});
+
 router.get('/admin', function(req, res) {
     connection.query('SELECT * FROM admin', function(error, result) {
+        if (error) {
+            console.log('ไม่สามารถดึงข้อมูล admin ได้');
+            throw error;
+        }
+        res.json(result);
+    });
+});
+
+router.get('/admin/:id', function(req, res) {
+    let id = req.params.id;
+    connection.query(`SELECT * FROM admin WHERE admin_id=${id}`, function(error, result) {
         if (error) {
             console.log('ไม่สามารถดึงข้อมูล admin ได้');
             throw error;
